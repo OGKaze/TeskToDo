bce6e883ff3b69f02fa3a6eaa3a517a1751d661d
kAze <kazeiswind26@gmail.com>
2026-05-01 14:07:42 +0700

    Pull Request Title: Add task creation window and integrate with main window UI
    Description:
    This PR introduces a new task creation window and implements the necessary interaction commands between the main application window and the new task window.
    
    Changes included:
    New task creation window
    A dedicated window interface has been added to allow users to create new tasks.
    
    Task window interface implementation
    Fully implemented the UI layout and logic for the task creation window.
    
    Interaction commands
    Added command handlers to manage communication and interaction with the task creation window.
    
    Main window buttons
    Implemented buttons on the main window to open and interact with the task creation window.
    
    Type of change:
    New feature (non-breaking change which adds functionality)

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,22 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TaskManagerMVVM.Views;
 
 namespace TaskManagerMVVM.ViewModels
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand IOpenNewWindow => new RelayCommand(OpenNewWindow);
+
+        private void OpenNewWindow()
+        {
+            NewTaskWindow newTaskWindow= new NewTaskWindow();
+            newTaskWindow.Show();
+        }
 
         protected virtual void OnPropertyChanged (string propertyName)
         {
